6a75a3fc5806fbda77efdb8b1b0f9ff8683beee8
carolinenee <caroline.nee@mail.utoronto.ca>
2025-03-10 18:13:02 -0400

    more stuff

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-mapboxgl.accessToken = "pk.eyJ1IjoiY2Fyb2xpbmVuZWUiLCJhIjoiY201b2RhZmxtMGthajJucHRxcW5heGxiNyJ9.NMKAQoQvhYJ8RQq0NQuYkA";
+mapboxgl.accessToken = "pk.eyJ1IjoiY2Fyb2xpbmVuZWUiLCJhIjoiY201b2RhZmxtMGthajJucHRxcW5heGxiNyJ9.NMKAQoQvhYJ8RQq0NQuYkA"; // token to use mapbox API 
 const map = new mapboxgl.Map({
     container: 'my-map', // map container ID
-    style: 'mapbox://styles/carolinenee/cm7uuha4i01rf01qo19chh4m6', // monochromatic basemap style 
-    center: [-79.39, 43.66], // starting position [lng, lat] of toronto
-    zoom: 13, // starting zoom
+    style: 'mapbox://styles/mapbox/standard', // standard mapbox style 
+    center: [-79.65, 43.6], // starting position [lng, lat] of peel
+    zoom: 10, // starting zoom
 });
 
 
 map.on('load', () => {
-    // Add your GeoJSON source
-    map.addSource('food_data', { //loading geojson cafe data made with geojson.io 
+    map.addSource('food_data', { //loading food program data 
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/carolinenee/nucleus/refs/heads/main/Food%20Services%20Locations%20and%20Day_Time%20Filters.geojson' // link to git hub raw data file
     });
-    // Add a layer for the locations
-    map.addLayer({
-      id: 'locations-layer',
+
+    map.addLayer({ //adding the food program data to the map 
+      id: 'locations-layer', 
       type: 'circle',
       source: 'food_data',
       paint: {
         'circle-radius': 5,
-        'circle-color': '#FF0000'
+        'circle-color': '#000000'
       }
     });
   });
