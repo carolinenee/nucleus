56c4ff4d3fbf98a85d5c0ea0d01822542f0afc83
mkbs-mkbs2000 <muhammadkhalisbinsamion@gmail.com>
2025-03-13 16:56:41 -0400

    add walk polygons

--- script.js
+++ script.js
@@ -2,38 +2,41 @@ mapboxgl.accessToken = "pk.eyJ1IjoiY2Fyb2xpbmVuZWUiLCJhIjoiY201b2RhZmxtMGthajJuc
 const map = new mapboxgl.Map({
     container: 'my-map', // map container ID
     style: 'mapbox://styles/mapbox/standard', // standard mapbox style 
-    center: [-79.65, 43.6], // starting position [lng, lat] of peel
-    zoom: 10, // starting zoom
+    center: [-79.7018518888638, 43.668552107715904], // starting position [lng, lat] of peel
+    zoom: 9, // starting zoom
 });
 
-
 map.on('load', () => {
-    map.addSource('food_data', { //loading food program data 
-        type: 'geojson',
-        data: 'https://raw.githubusercontent.com/carolinenee/nucleus/refs/heads/main/Food%20Services%20Locations%20and%20Day_Time%20Filters.geojson' // link to git hub raw data file
-    });
+  
+  //loading food program data
+  map.addSource('food_data', { 
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/carolinenee/nucleus/refs/heads/main/Food%20Services%20Locations%20and%20Day_Time%20Filters.geojson' // link to git hub raw data file
+  });
 
-    map.addLayer({ //adding the food program data to the map 
-        id: 'locations-layer',
-        type: 'circle',
-        source: 'food_data',
-        paint: {
-            'circle-radius': 5,
-            'circle-color': '#000000'
-        }
+  //adding the food program data to the map 
+  map.addLayer({
+    id: 'locations-layer',
+    type: 'circle',
+    source: 'food_data',
+    paint: {
+      'circle-radius': 5,
+      'circle-color': '#000000'
+    }
+  });
+
+  document.querySelectorAll('#filters button').forEach(button => {
+    button.addEventListener('click', () => {
+      const selectedDay = button.getAttribute('data-day');
+      filterByDays([selectedDay]); // Pass the selected day as an array
     });
-   document.querySelectorAll('#filters button').forEach(button => {
-        button.addEventListener('click', () => {
-          const selectedDay = button.getAttribute('data-day');
-          filterByDays([selectedDay]); // Pass the selected day as an array
-        });
-      });
+  });
 });
 
 function filterByDays(days) {
-    const filterConditions = ['all']; // Start with an "and" condition
-    days.forEach(day => {
-      filterConditions.push(['==', ['get', day], 1]);
-    });
-    map.setFilter('locations-layer', filterConditions);
-  }
+  const filterConditions = ['all']; // Start with an "and" condition
+  days.forEach(day => {
+    filterConditions.push(['==', ['get', day], 1]);
+  });
+  map.setFilter('locations-layer', filterConditions);
+}
